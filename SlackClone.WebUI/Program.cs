b9ebffd15a97c364bfa8340d36fdc6f439e8f2ed
JustinErdmier <justin.erdmier@outlook.com>
2023-01-11 20:16:47 -0600

    :tada: initial commit

---
+++ SlackClone.WebUI/Program.cs
@@ -0,0 +1,22 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+WebApplication app = builder.Build();
+
+if (! app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
---
+++ SlackClone.WebUI/tailwind.config.js
@@ -0,0 +1,22 @@
+﻿module.exports = {
+    content: [
+        './**/*.{cshtml,razor}',
+    ],
+    theme:   {
+        extend: {
+            colors:     {
+                'slackBg':  '#3e103f',
+                'slackNav': '#2b092a',
+            },
+            fontFamily: {
+                sans: [
+                    'Montserrat',
+                    'sans-serif'
+                ]
+            },
+        }
+    },
+    plugins: [
+        require('tailwindcss-textshadow'),
+    ]
+};
